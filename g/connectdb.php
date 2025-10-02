cfd5129b5e5a1f0f2609c22e7cde21b6031355ae
Bancha04 <67010974008@msu.ac.th>
2025-10-02 18:54:45 +0700

    e

--- g/connectdb.php
+++ g/connectdb.php
@@ -2,8 +2,8 @@
 <?php
 	$host = "localhost" ;
 	$user = "root" ;
-	$pwd = "" ;
-	$dbname = "msu" ;
+	$pwd = "Mbs12345" ;
+	$dbname = "msu_bancha" ;
 	$conn = mysqli_connect($host, $user, $pwd, $dbname) ;
 	mysqli_query($conn, "SET NAMES utf8")
 	?>
